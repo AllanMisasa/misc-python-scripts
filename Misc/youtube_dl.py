822280f464593d971de90d668727592671a50b6a
Allan Misasa Nielsen <Singulytics@gmail.com>
2024-02-21 15:05:18 +0100

    FFMPEG to combine audio and video added to youtube downloader

--- Misc/youtube_dl.py
+++ Misc/youtube_dl.py
@@ -1,24 +1,48 @@
 '''
 This script is used to download videos from YouTube
 '''
-
+import ffmpeg
+import os
 import sys
 import pytube
 from pytube.cli import on_progress
 
-def download_video(url, res='1440p'):
+def download_video(url, resolution):
     '''Download a video from YouTube'''
     try:
-        yt = pytube.YouTube(url=url, on_progress_callback=on_progress, use_oauth=True,
-        allow_oauth_cache=True)
+        yt = pytube.YouTube(url=url, on_progress_callback=on_progress, use_oauth=True, allow_oauth_cache=True)
         print("Trying to download " + yt.title)
-        video = yt.streams.filter(res=res).first()
-        print("Downloading...")
+        video = yt.streams.filter(res=resolution).first()
+        print("Downloading...", video)
         video.download()
         print("Download complete")
     except:
         print("Error in download_video")
         sys.exit(1)
 
-url = "https://www.youtube.com/watch?v=FfcIKP2bscQ"
-download_video(url, '1080p')
+def download_audio(url):
+    '''Download the audio from a video'''
+    try:
+        yt = pytube.YouTube(url=url, on_progress_callback=on_progress, use_oauth=True, allow_oauth_cache=True)
+        print("Trying to download " + yt.title)
+        audio = yt.streams.filter(only_audio=True, abr="160kbps").first()
+        print("Downloading...", audio)
+        audio.download()
+        print("Download complete")
+    except:
+        print("Error in download_audio")
+        sys.exit(1)
+
+def combine_audio_video(video, audio, output="output.mp4"):
+    try:
+        video = ffmpeg.input(video)
+        audio = ffmpeg.input(audio)
+        ffmpeg.output(video, audio, output).run()
+    except:
+        print("Error in combine_audio_video")
+        sys.exit(1) 
+
+url = "https://www.youtube.com/watch?v=dxYY097QNs0"
+download_video(url, resolution="1080p")
+download_audio(url)
+combine_audio_video("LoRa CHIRP.mp4", "LoRa CHIRP.webm", "lora.mp4")
